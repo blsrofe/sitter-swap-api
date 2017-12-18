5127838246c486a4bf8df528fc262b0721dc0dfe
Becki Srofe <blf_23@yahoo.com>
2017-12-18 16:50:11 -0700

    Add endpoint for single user

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const environment = process.env.NODE_ENV || 'development'
+const configuration = require('./knexfile')[environment]
+const database = require('knex')(configuration)
+const UsersController = require('./lib/controllers/users')
+
+app.set('port', process.env.PORT || 3000)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+  next()
+})
+
+app.get('/', (request, response) => {
+  response.send('API for SitterSwap')
+})
+
+app.get('/api/v1/users/:id', UsersController.getUser)
+
+if (!module.parent) {
+  app.listen(app.get('port'), () => {
+    console.log(`App running on ${app.get('port')}.`)
+  })
+}
+
+module.exports = app
