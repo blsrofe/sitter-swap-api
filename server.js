45f827c4b4f0c4577804cdafd2ad18b28cafb04f
Becki Srofe <blf_23@yahoo.com>
2017-12-26 10:21:34 -0700

    Fix dogs route

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/v1/users/:id', UsersController.getUser)
-app.post('/api/v1/users/:id/dogs', DogsController.postDog)
+app.post('/api/v1/dogs', DogsController.postDog)
 app.post('/api/v1/users', UsersController.postUser)
 
 if (!module.parent) {
